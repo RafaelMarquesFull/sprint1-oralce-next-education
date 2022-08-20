192a7ffe86375de3ccefd84f5788c4fbd4e02d3c
Rafael <rafafismed@icloud.com>
2022-08-20 11:11:17 -0300

    refatoração

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,41 @@ function startEncrip() {
 // função de encriptação
 
 function encriptador(dados) {
-  if (dados.includes('a')) {
-    var rest = dados.replaceAll('a', 'ai');
-    if (rest.includes('e')) {
-      rest = rest.replaceAll('e', 'enter');
-      if (rest.includes('i')) {
-        rest = rest.replaceAll('i', 'imes');
-        if (rest.includes('o')) {
-          rest = rest.replaceAll('o', 'ober');
-          if (rest.includes('u')) {
-            rest = rest.replaceAll('u', 'ufat');
-          }
-        }
-      }
+  // if (dados.includes('a')) {
+  //   var rest = dados.replaceAll('a', 'ai');
+  //   if (rest.includes('e')) {
+  //     rest = rest.replaceAll('e', 'enter');
+  //     if (rest.includes('i')) {
+  //       rest = rest.replaceAll('i', 'imes');
+  //       if (rest.includes('o')) {
+  //         rest = rest.replaceAll('o', 'ober');
+  //         if (rest.includes('u')) {
+  //           rest = rest.replaceAll('u', 'ufat');
+  //         }
+  //       }
+  //     }
+  //   }
+  // }
+
+  // return rest;
+  let matrizCodigo = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+  ];
+  stringEncriptada = dados.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringEncriptada.includes(matrizCodigo[i][0])) {
+      stringEncriptada = stringEncriptada.replaceAll(
+        matrizCodigo[i][0],
+        matrizCodigo[i][1]
+      );
     }
   }
-
-  return rest;
+  return stringEncriptada;
 }
 
 //quando clicado
@@ -59,23 +77,41 @@ function startDesencrip() {
 //Função desencriptadora
 
 function desencriptador(dados) {
-  if (dados.includes('ia')) {
-    var rest = dados.replaceAll('ia', 'a');
-    if (rest.includes('enter')) {
-      rest = rest.replaceAll('enter', 'e');
-      if (rest.includes('imes')) {
-        rest = rest.replaceAll('imes', 'i');
-        if (rest.includes('ober')) {
-          rest = rest.replaceAll('ober', 'o');
-          if (rest.includes('ufat')) {
-            rest = rest.replaceAll('ufat', 'u');
-          }
-        }
-      }
+  //if (dados.includes('ia')) {
+  //  var rest = dados.replaceAll('ia', 'a');
+  //  if (rest.includes('enter')) {
+  //    rest = rest.replaceAll('enter', 'e');
+  //    if (rest.includes('imes')) {
+  //     rest = rest.replaceAll('imes', 'i');
+  //      if (rest.includes('ober')) {
+  //       rest = rest.replaceAll('ober', 'o');
+  //        if (rest.includes('ufat')) {
+  //          rest = rest.replaceAll('ufat', 'u');
+  //       }
+  //      }
+  //    }
+  //  }
+  // }
+  // return rest;
+  let matrizCodigo = [
+    ['e', 'enter'],
+    ['i', 'imes'],
+    ['a', 'ai'],
+    ['o', 'ober'],
+    ['u', 'ufat'],
+  ];
+  stringDesencriptada = dados.toLowerCase();
+
+  for (let i = 0; i < matrizCodigo.length; i++) {
+    if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+      stringDesencriptada = stringDesencriptada.replaceAll(
+        matrizCodigo[i][1],
+        matrizCodigo[i][0]
+      );
     }
   }
 
-  return rest;
+  return stringDesencriptada;
 }
 
 function clikDesencriptar() {
@@ -84,7 +120,6 @@ function clikDesencriptar() {
 
   var criptoFinal = desencriptador(dadosDesencriptados.value);
   result.textContent = criptoFinal;
-  alert('testebotão');
 }
 
 startEncrip();
